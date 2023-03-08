ec008bc3ca76ca5581b0fbf7a243f9dca5111aad
Sangay Thinley <thinley.sangay@selise.ch>
2023-03-08 13:46:00 +0600

    creating dummy pages

---
+++ src/pages/create-playlist/create-playlist.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+const CreatePlaylist = () => {
+
+    return (
+        <Box sx={{display: 'flex'}}>
+            <p>create playlist Page</p>
+        </Box>
+    );
+}
+
+export default CreatePlaylist;
