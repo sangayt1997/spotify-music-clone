68dcb006793b734ff9c6adbb9c6267aef065c444
Sangay Thinley <thinley.sangay@selise.ch>
2023-03-09 15:43:08 +0600

    adding cards in home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import { Card, CardContent } from "../../shared/styles/components.style";
+import dummyImage from "../../asset/images/spotify_text_image.png";
 
 const Home = () => {
 
     return (
         <Box sx={{display: 'flex'}}>
-            <p>Home Page</p>
+            <Card>
+                <img src={dummyImage} alt="dummy" />
+                <CardContent>
+                    <h2>fnsdfnnsfdfdjsn</h2>
+                    <p>ndfsdfnfjdfnfnsdjf</p>
+                </CardContent>
+            </Card>
         </Box>
     );
 }
