36d656895bfd1d48382a042970c18e053beb0c1a
Sangay Thinley <thinley.sangay@selise.ch>
2023-03-16 16:17:01 +0600

    styling the albums listing table

--- src/components/albums-table/albums-table.tsx
+++ src/components/albums-table/albums-table.tsx
@@ -1,65 +1,122 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { AlbumsTableContainer } from "./styles/ablums-table.style";
+import Stack from "@mui/material/Stack";
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 
 const AlbumsTable = () => {
     const rows = [
         {
             id: 1,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884847/spotify-clone/listening_vibe_music_fjzqxl.webp',
             title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
         },
         {
             id: 3,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884961/spotify-clone/red_girl_cwefa0.webp',
             title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
         },
         {
             id: 4,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884897/spotify-clone/music_album_banner_m6is6d.webp',
             title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
         },
         {
             id: 5,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678885024/spotify-clone/vibe_music_jcfe9r.webp',
             title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
         },
         {
             id: 6,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884764/spotify-clone/joyful_melody_l9bbpd.webp',
             title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
         },
         {
             id: 7,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884665/spotify-clone/guy_music_eatj4r.webp',
             title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
         },
         {
             id: 8,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884847/spotify-clone/listening_vibe_music_fjzqxl.webp',
             title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
         },
         {
             id: 9,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884961/spotify-clone/red_girl_cwefa0.webp',
             title: 'Run',
+            artist: 'OneRepublic',
+            album: 'Run',
+            dateAdded: '1 week ago',
+            duration: '2:49'
+        },
+        {
+            id: 10,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884764/spotify-clone/joyful_melody_l9bbpd.webp',
+            title: 'Run',
+            artist: 'OneRepublic',
+            album: 'Run',
+            dateAdded: '1 week ago',
+            duration: '2:49'
+        },
+        {
+            id: 11,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884665/spotify-clone/guy_music_eatj4r.webp',
+            title: 'Run',
+            artist: 'OneRepublic',
+            album: 'Run',
+            dateAdded: '1 week ago',
+            duration: '2:49'
+        },
+        {
+            id: 12,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884847/spotify-clone/listening_vibe_music_fjzqxl.webp',
+            title: 'Run',
+            artist: 'OneRepublic',
+            album: 'Run',
+            dateAdded: '1 week ago',
+            duration: '2:49'
+        },
+        {
+            id: 13,
+            albumImage: 'https://res.cloudinary.com/dsim60jne/image/upload/v1678884961/spotify-clone/red_girl_cwefa0.webp',
+            title: 'Run',
+            artist: 'OneRepublic',
             album: 'Run',
             dateAdded: '1 week ago',
             duration: '2:49'
@@ -71,11 +128,13 @@ const AlbumsTable = () => {
             <Table>
                 <TableHead>
                     <TableRow className="none-border">
-                        <TableCell>#</TableCell>
-                        <TableCell>Title</TableCell>
-                        <TableCell>Album</TableCell>
-                        <TableCell>Date Added</TableCell>
-                        <TableCell>music duration icon</TableCell>
+                        <TableCell className="color--gray">#</TableCell>
+                        <TableCell className="color--gray">Title</TableCell>
+                        <TableCell className="color--gray">Album</TableCell>
+                        <TableCell className="color--gray">Date Added</TableCell>
+                        <TableCell className="color--gray">
+                            <ScheduleIcon fontSize="small"/>
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -84,10 +143,30 @@ const AlbumsTable = () => {
                             key={item.id}
                         >
                             <TableCell>{item.id}</TableCell>
-                            <TableCell>{item.title}</TableCell>
-                            <TableCell>{item.album}</TableCell>
-                            <TableCell>{item.dateAdded}</TableCell>
-                            <TableCell>{item.duration}</TableCell>
+                            <TableCell>
+                                <Stack direction="row" spacing={2} alignItems="center">
+                                    <img width="40" height="40" src={item.albumImage} alt="album"/>
+                                    <Stack>
+                                        <Link to="/:id" className="fs-16">{item.title}</Link>
+                                        <Link to="/:id" className="artist-title color--gray">{item.artist}</Link>
+                                    </Stack>
+                                </Stack>
+                            </TableCell>
+                            <TableCell>
+                                <Link to="/:id" className="color--gray">
+                                    {item.album}
+                                </Link>
+                            </TableCell>
+                            <TableCell className="color--gray">{item.dateAdded}</TableCell>
+                            <TableCell>
+                                <Stack direction="row" alignItems="center" spacing={7}>
+                                    <FavoriteBorderOutlinedIcon fontSize="small" className="color--gray"/>
+                                    <Stack direction="row" alignItems="center" spacing={3}>
+                                        <p className="color--gray">{item.duration}</p>
+                                        <MoreHorizOutlinedIcon fontSize="small"/>
+                                    </Stack>
+                                </Stack>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
--- src/components/albums-table/styles/ablums-table.style.tsx
+++ src/components/albums-table/styles/ablums-table.style.tsx
@@ -2,12 +2,17 @@ import styled from "styled-components";
 import TableContainer from "@mui/material/TableContainer";
 
 export const AlbumsTableContainer = styled(TableContainer)`
+  .artist-title {
+    font-size: 14px;
+  }
   th {
     border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+    padding: 12px 6px;
   }
 
   td {
     border: none;
+    padding: 8px;
   }
   
   tr {
